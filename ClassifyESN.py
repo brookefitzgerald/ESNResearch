3074deeba0c9ac09bed37fc1703a607259e1c21b
brookefitzgerald <brooke.anne.fitzgerald@gmail.com>
2017-05-10 15:24:36 +0200

    Wrote and tested function to create teacher signals

--- ClassifyESN.py
+++ ClassifyESN.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 from os import listdir,getcwd
+from functools import partial
 
 zhang_dir = getcwd()+'\\ZhangData'
-data = np.zeros((132,6), dtype=[('neuron_id','a13'),
+data = np.zeros(132, dtype=[('neuron_id','a13'),
 								('raster_data',np.float32,(419,1000)),
 								('raster_info','a20',4),
 								('stimulus_id','a15',419),
@@ -21,15 +22,14 @@ for i,data_dir in enumerate(listdir(zhang_dir)):
 	data[i] = (neuron_id, data_arr, pd.read_csv(raster_info).values, labels_arr[:,0],labels_arr[:,1],labels_arr[:,2])
 
 # Getting only the neurons measured together during run number 1018
-index_1018=data['raster_info'][:,0,3]=='1018'
-data_1018 = np.zeros((11,6), dtype=[('neuron_id','a13'),
+index_1018=data['raster_info'][:,3]=='1018'
+data_1018 = np.zeros(11, dtype=[('neuron_id','a13'),
 								('raster_data',np.float32,(419,1000)),
 								('raster_info','a20',4),
 								('stimulus_id','a15',419),
 								('stimulus_position','a15',419),
 								('combined_id_position','a15',419)])
-for i in np.arange(11):
-	data_1018[i] = data[index_1018]
+data_1018[np.arange(11)] = data[index_1018]
 
 def getSpecificStimulus(sid, neuron_index = None, d=data):
 	if neuron_index==None:
@@ -38,9 +38,8 @@ def getSpecificStimulus(sid, neuron_index = None, d=data):
 		to_return = [x['raster_data'][x[0]==sid] for x in d[['stimulus_id','raster_data']][neuron_index][:]]
 	return(to_return)
 
-def addExponentialDecayToNeuronRead(rate_of_exp_decrease, neuron_read):
+def addExponentialDecayToNeuronRead(neuron_read, rate_of_exp_decrease):
     activation = 0.0
-    to_return = np.zeros(len(neuron_read))
     for i,x in enumerate(neuron_read):
         if x==1.0:
             activation+=1.0
@@ -48,12 +47,28 @@ def addExponentialDecayToNeuronRead(rate_of_exp_decrease, neuron_read):
             activation *= rate_of_exp_decrease
         if activation<0.:
             activation=0.
-        to_return[i]=activation
-
-    return(to_return)
+        neuron_read[i]=activation
 
 
 def addExponentialDecay(speed,d=data):
-	raster_data =  d['raster_data'][:]
-	data = np.apply(addExponentialDecayToNeuronRead, axis = 0)
+    addExpDecay = partial(addExponentialDecayToNeuronRead, rate_of_exp_decrease = speed)
+    for neuron in d:
+        np.apply_along_axis(addExpDecay, axis = 1, arr = neuron['raster_data'])
+
+def createTeacherSignal(d=data):
+	ind_dict = dict(zip(['car', 'couch', 'face', 'flower', 'guitar', 'hand', 'kiwi'],np.arange(7)))
+	teacher_signal = np.zeros(len(d), dtype = (np.float32,(7,419,1000)))
+	increase = [1.0/(1.0+np.exp(-0.7*x)) for x in np.linspace(-10,10,100)]
+	decrease = [1.0/(1.0+np.exp(0.7*x)) for x in np.linspace(-10,10,100)]
+	for i, neuron in enumerate(d):
+		for j in np.arange(419):
+			stimulus_index = ind_dict[neuron['stimulus_id'][j]]
+			stimulus_shown = int(neuron['raster_info'][0])
+			teacher_signal[stimulus_index][i][stimulus_shown:stimulus_shown+100] = increase
+			teacher_signal[stimulus_index][i][stimulus_shown+100:stimulus_shown+200] = decrease
+
+	return (teacher_signal)
+
+data_exp_decay_1018 = data_1018.copy()
+addExponentialDecay(0.995,data_exp_decay_1018)
 
